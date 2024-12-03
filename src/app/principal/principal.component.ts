cb0e7f159d0a7abef4578d216f0774d9e52eb681
JaimeConesa <168517014+JaimeConesa@users.noreply.github.com>
2024-12-03 11:09:31 +0100

    G

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./principal.component.scss']
 })
 export class PrincipalComponent {
-
+  salsas = [
+    {
+      name: 'Ketchup',
+      img: 'https://res.cloudinary.com/kraft-heinz-whats-cooking-ca/image/upload/f_auto/q_auto/c_limit,w_1920/f_auto/q_auto/v1/dxp-images/heinz/International/Master/pg-Home/Spain/Property_1_ml-card-content-ketchup_l4cksz?_a=BAVAfVIB0'
+    },
+    {
+      name: 'Mostaza',
+      img: 'https://res.cloudinary.com/kraft-heinz-whats-cooking-ca/image/upload/f_auto/q_auto/c_limit,w_1920/f_auto/q_auto/v1/dxp-images/heinz/International/Master/pg-Home/Spain/Property_1_ml-card-content-mostaza_yygraw?_a=BAVAfVIB0'
+    },
+    {
+      name: 'Mayonesa',
+      img: 'https://res.cloudinary.com/kraft-heinz-whats-cooking-ca/image/upload/f_auto/q_auto/c_limit,w_1920/f_auto/q_auto/v1/dxp-images/heinz/International/Master/pg-Home/Spain/Property_1_ml-card-content-mayo_lqbbi0?_a=BAVAfVIB0'
+    },
+    {
+      name: 'Barbacoa',
+      img: 'https://res.cloudinary.com/kraft-heinz-whats-cooking-ca/image/upload/f_auto/q_auto/c_limit,w_1920/f_auto/q_auto/v1/dxp-images/heinz/International/Master/pg-Home/Spain/Property_1_ml-card-content-salsa_yw3kts?_a=BAVAfVIB0'
+    }
+  ];
 }
