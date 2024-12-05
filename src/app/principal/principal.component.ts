2be9732f5c34e244e41e2eee47cdac930ed8a6e6
Jaime Conesa Ponce <jaimeconesa.23@campuscamara.es>
2024-12-05 02:10:34 +0100

    w
    d

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -24,4 +24,11 @@ export class PrincipalComponent {
       img: 'https://res.cloudinary.com/kraft-heinz-whats-cooking-ca/image/upload/f_auto/q_auto/c_limit,w_1920/f_auto/q_auto/v1/dxp-images/heinz/International/Master/pg-Home/Spain/Property_1_ml-card-content-salsa_yw3kts?_a=BAVAfVIB0'
     }
   ];
+  botes = [
+    { id: 'k1', img: '../../assets/img/k1.png' },
+    { id: 'k2', img: '../../assets/img/k2.png' },
+    { id: 'k3', img: '../../assets/img/k3.png' },
+    { id: 'k4', img: '../../assets/img/k4.png' },
+    { id: 'k5', img: '../../assets/img/k5.png' }
+  ];
 }
